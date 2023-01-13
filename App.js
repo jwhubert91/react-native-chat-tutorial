f2c9809dc76e4b244e1379e306158a4b431613ab
James Hubert <jwhubert91@gmail.com>
2023-01-13 08:46:49 -0500

    implement auth with context

--- App.js
+++ App.js
@@ -1,27 +1,81 @@
-import React from "react"
+import React, { createContext, useContext, useEffect, useState } from "react"
+import { View, ActivityIndicator } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
+import { onAuthStateChanged } from "firebase/auth"
+
+// Components
 import Chat from "./screens/Chat"
 import Login from "./screens/Login"
+import SignUp from "./screens/SignUp"
+import Home from "./screens/Home"
+
+import { auth } from "./config/firebase"
 
 const Stack = createStackNavigator()
+const AuthenticatedUserContext = createContext({})
+
+const AuthenticatedUserProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+  return (
+    <AuthenticatedUserContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthenticatedUserContext.Provider>
+  )
+}
 
 function ChatStack() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator defaultScreenOptions={Home}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Chat" component={Chat} />
+    </Stack.Navigator>
+  )
+}
+
+function AuthStack() {
+  return (
+    <Stack.Navigator
+      defaultScreenOptions={Login}
+      screenOptions={{ headerShown: false }}
+    >
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="SignUp" component={SignUp} />
     </Stack.Navigator>
   )
 }
 
 function RootNavigator() {
+  const { user, setUser } = useContext(AuthenticatedUserContext)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (authenticatedUser) => {
+      authenticatedUser ? setUser(authenticatedUser) : setUser(null)
+      setLoading(false)
+    })
+    return () => unsubscribe()
+  }, [user])
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  }
+
   return (
     <NavigationContainer>
-      <ChatStack />
+      {user ? <ChatStack /> : <AuthStack />}
     </NavigationContainer>
   )
 }
 
 export default function App() {
-  return <RootNavigator />
+  return (
+    <AuthenticatedUserProvider>
+      <RootNavigator />
+    </AuthenticatedUserProvider>
+  )
 }
--- screens/Login.js
+++ screens/Login.js
@@ -31,6 +31,51 @@ export default function Login({ navigation }) {
     <View style={styles.container}>
       <Image source={backImage} style={styles.backImage} />
       <View style={styles.whiteSheet} />
+      <SafeAreaView style={styles.form}>
+        <Text style={styles.title}>Login</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter email"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+          autoFocus={true}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter password"
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry={true}
+          textContentType="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <TouchableOpacity style={styles.button} onPress={onHandleLogin}>
+          <Text style={{ fontWeight: "bold", color: "#fff", fontSize: 18 }}>
+            Log In
+          </Text>
+        </TouchableOpacity>
+        <View
+          style={{
+            marginTop: 20,
+            flexDirection: "row",
+            alignItems: "center",
+            alignSelf: "center",
+          }}
+        >
+          <Text style={{ color: "gray", fontWeight: "600", fontSize: 14 }}>
+            Don't have an account?{" "}
+          </Text>
+          <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
+            <Text style={{ fontWeight: "600", color: "#f57c00", fontSize: 14 }}>
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     </View>
   )
 }
